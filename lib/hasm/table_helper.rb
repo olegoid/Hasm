eeb91625e9e6c5de658071b54de707c68f4fdc19
olegoid <olegoid91@gmail.com>
2016-06-26 22:51:22 -0300

    Table helper implementation

--- lib/hasm/table_helper.rb
+++ lib/hasm/table_helper.rb
@@ -1,2 +1,14 @@
-class TableHelper
+require 'terminal-table'
+
+module Hasm
+  class TableHelper
+    def self.create_table(header, items)
+      rows = []
+      items.each do |item|
+        rows << [item]
+      end
+
+      Terminal::Table.new :headings => [header], :rows => rows
+    end
+  end
 end
